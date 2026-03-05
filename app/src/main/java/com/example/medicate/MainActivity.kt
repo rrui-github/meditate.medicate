555d1bf39537a7932956663ed0a793424a5cc68b
rrui <setiaadiprayoga@gmail.com>
2026-03-05 13:08:36 +0700

    Adding GPL V2 header

--- app/src/main/java/com/example/medicate/MainActivity.kt
+++ app/src/main/java/com/example/medicate/MainActivity.kt
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2024 setia
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 package com.example.medicate
 
 import android.os.Bundle
@@ -278,7 +296,7 @@ fun WeekView(logs: List<MeditationSession>) {
     val weeklyData = remember(logs) {
         val calendar = Calendar.getInstance()
         calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY)
-        (0..6).map { i ->
+        (0..6).map { _ ->
             val d = calendar.timeInMillis
             val totalSecs = logs.filter { isSameDay(it.timestamp, d) }.sumOf { it.durationSeconds }
             calendar.add(Calendar.DAY_OF_YEAR, 1)
